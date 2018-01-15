68935c5f13a6ebb01ae9a800d2566b392ca9e03d
AnthonyGW <anthony.waithaka@andela.com>
2018-01-15 11:28:46 +0300

    fix: removed sudo commands

--- ci/test_script.sh
+++ ci/test_script.sh
@@ -3,13 +3,8 @@
 set -o errexit
 set -o nounset
 
-sudo apt install -yv python3-pip
-sudo apt-get install -yv postgresql postgresql-contrib
-sudo apt-get install -yv python-psycopg2
-sudo apt-get install -yv libpq-dev
-
 export APP_SETTINGS="testing"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 python3 manage.py db init
 python3 manage.py db migrate
