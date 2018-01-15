d21d76c34729dd4e3ee4af81ed6ae725cce01423
AnthonyGW <anthony.waithaka@andela.com>
2018-01-15 12:01:37 +0300

    fix: remove pkg install commands

--- ci/test_script.sh
+++ ci/test_script.sh
@@ -3,13 +3,6 @@
 set -o errexit
 set -o nounset
 
-apt-get install -yv python3-pip
-apt-get install -yv git
-
-git clone https://github.com/AnthonyGW/ConcourseProject.git
-
-cd ConcourseProject
-
 export APP_SETTINGS="testing"
 pip3 install -r requirements.txt
 
