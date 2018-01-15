2da14340612ed6a391783602fe32154e139201e2
AnthonyGW <anthony.waithaka@andela.com>
2018-01-15 10:03:01 +0300

    fix: add pip install line

--- test_script.sh
+++ test_script.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 
 export APP_SETTINGS="testing"
+pip3 install -r requirements.txt
+
 python3 manage.py db init
 python3 manage.py db migrate
 python3 manage.py db migrate
