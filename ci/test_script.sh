b98c497cb75130a0a8070a0b73bf46888edf6161
AnthonyGW <anthony.waithaka@andela.com>
2018-01-15 11:37:24 +0300

    fix: change back to pip3

--- ci/test_script.sh
+++ ci/test_script.sh
@@ -6,7 +6,7 @@ set -o nounset
 apt-get install -yv python3-pip
 
 export APP_SETTINGS="testing"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 python3 manage.py db init
 python3 manage.py db migrate
